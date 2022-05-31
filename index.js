851f406dd712a41610665a3716c40df9001f37e3
zaieon <zoejohn007@gmail.com>
2022-05-31 11:13:37 +0100

    Color game

---
+++ index.js
@@ -0,0 +1,32 @@
+// let newColours = document.querySelectorAll('a')
+// let hardBox = document.querySelectorAll('.hard')
+// console.log(newColours)
+
+// newColours[1].addEventListener('click', function () {
+//     for (let i = 0; i < hardBox.length; i++) {
+//         hardBox[i].classList.add('remove')
+//     }
+// })
+
+// newColours[2].addEventListener('click', function () {
+//     for (let i = 0; i < hardBox.length; i++) {
+//         hardBox[i].classList.remove('remove')
+//     }
+// })
+
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min) + min);
+// }
+
+// let colours = document.querySelectorAll('.colours')
+
+// for (let i = 0; i < colours.length; i++) {
+//     colours[i].style.backgroundColor = 'rgb(244, 124, 202)'
+
+// }
+
+// console.log(getRandomInt(1, 5))
+
+
